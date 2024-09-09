653856a86772493f1e9a52bf5227b8b78d6e7ae2
Adrian Basilio López <BL_980713@hotmail.com>
2024-09-08 21:34:16 -0600

    Add Light Mode
    Añadido el modo Claro y Transiciones

--- assets/js/script.js
+++ assets/js/script.js
@@ -40,3 +40,25 @@ TweenMax.from(".figura", 1.5, {
     opacity: 0,
     ease: Expo.easeInOut
 })
+
+// light mode - modo claro
+let lightmode = localStorage.getItem("light-mode")
+const temaSwitch = document.getElementById("tema-switch")
+
+const habilitarModoClaro = () => {
+    document.body.classList.add("light-mode")
+    localStorage.setItem("light-mode", "activo")
+}
+
+const deshabilitarModoClaro = () => {
+    document.body.classList.remove("light-mode")
+    document.body.style.transition = "1s"
+    localStorage.setItem("light-mode", null)
+}
+
+if(lightmode === "activo") habilitarModoClaro()
+
+temaSwitch.addEventListener("click", () => {
+    lightmode = localStorage.getItem("light-mode")
+    lightmode !== "activo" ? habilitarModoClaro() : deshabilitarModoClaro()
+})
